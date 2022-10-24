727c199e885662bcb88253586812c32a13212d18
darksonok@yandex.ru <darksonok@yandex.ru>
2022-10-24 18:25:59 +0300

    Исправлены баги с фокусом на попапах

--- src/components/add-item-modal/add-item-modal.tsx
+++ src/components/add-item-modal/add-item-modal.tsx
@@ -58,7 +58,11 @@ function AddItemModal ( { cameraCard, onCloseButtonClick }: AddItemModalProps) {
             </div>
           </div>
           <div className="modal__buttons">
-            <button className="btn btn--purple modal__btn modal__btn--fit-width" type="button">
+            <button
+              className="btn btn--purple modal__btn modal__btn--fit-width"
+              type="button"
+              autoFocus
+            >
               <svg width="24" height="16" aria-hidden="true">
                 <use xlinkHref="#icon-add-basket"></use>
               </svg>Добавить в корзину
